44d66293dfb25b63d6cbbf3af9f5f860939ac66b
Clément Rigo <rigoclement@mydnic.be>
2026-05-13 08:47:01 +0200

    fix

--- src/KanpenFilamentPlugin.php
+++ src/KanpenFilamentPlugin.php
@@ -2,4 +2,41 @@
 
 namespace Mydnic\KanpenFilamentPlugin;
 
-class KanpenFilamentPlugin {}
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+use Mydnic\KanpenFilamentPlugin\Resources\CampaignResource;
+use Mydnic\KanpenFilamentPlugin\Resources\SubscriberResource;
+
+class KanpenFilamentPlugin implements Plugin
+{
+    public function getId(): string
+    {
+        return 'kanpen-filament-plugin';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel->resources([
+            CampaignResource::class,
+            SubscriberResource::class,
+        ]);
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public static function get(): static
+    {
+        /** @var static $plugin */
+        $plugin = filament(app(static::class)->getId());
+
+        return $plugin;
+    }
+}
